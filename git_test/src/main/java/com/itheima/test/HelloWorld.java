07ee703000a63cbcf3336e7607b7d2def0e4cfc1
zwj <1066314951@qq.com>
2019-06-24 20:04:23 +0800

    修改HelloWorld类2

--- git_test/src/main/java/com/itheima/test/HelloWorld.java
+++ git_test/src/main/java/com/itheima/test/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("hahaha ");
+        System.out.println("hahaha ");
     }
 }
